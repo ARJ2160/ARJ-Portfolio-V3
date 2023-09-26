6439f96df39484f2e2ad332f58e76e7394eddc29
Atharva <atharvaj2160@gmail.com>
2023-09-26 22:52:10 +0530

    V3 Initial Commit

---
+++ context/theme.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { createContext, useContext, useState } from "react";
+
+export type Theme = "light" | "dark";
+
+type ThemeContextProviderProps = {
+  children: React.ReactNode;
+};
+
+export type ThemeContextType = {
+  theme: Theme;
+  toggleTheme: () => void;
+};
+
+const ThemeContext = createContext<ThemeContextType | null>(null);
+
+export const ThemeContextProvider = ({
+  children,
+}: ThemeContextProviderProps) => {
+  const [theme, setTheme] = useState<Theme>("light");
+
+  // Simple toggle function to switch states
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+      localStorage.setItem("theme", "dark");
+      document.documentElement.classList.add("dark");
+    } else {
+      setTheme("light");
+      window.localStorage.setItem("theme", "light");
+      document.documentElement.classList.remove("dark");
+    }
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
