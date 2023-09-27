44d2a635c97939b4932656fad7f12d9ba4669bea
Atharva <atharvaj2160@gmail.com>
2023-09-27 12:43:00 +0530

    fix build error

---
+++ app/_document.tsx
@@ -0,0 +1,42 @@
+// import { Html, Head, Main, NextScript } from "next/document";
+
+// export default function Document() {
+//   return (
+//     <Html>
+//       <Head>
+//         <link rel="manifest" href="/manifest.json" />
+//         <link rel="apple-touch-icon" href="/icon.png" />
+//         <link rel="theme-color" href="#fff" />
+//         <meta name="viewport" content="width=device-width, initial-scale=1" />
+//         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+//         <meta property="og:url" content="https://www.atharvaj.online" />
+//         <meta property="og:type" content="website" />
+//         <meta property="og:site_name" content="ARJs Portfolio V3" />
+//         <meta property="og:title" content="ARJs Portfolio V3" />
+//         <meta itemProp="name" content="ARJs Portfolio V3" />
+//         <meta name="author" content="Atharva Joshi" />
+//         <meta name="language" content="en-us" />
+//         <meta name="description" content="ARJs Portfolio Website Version 3" />
+//         <meta name="theme-color" content="#3399FF" />
+//         <meta charSet="utf-8" />
+//         <meta
+//           itemProp="image"
+//           content="https://ik.imagekit.io/36athv2v82c8/Screenshot_2022-12-18_125952_xWZQNyd3I.png?ik-sdk-version=javascript-1.4.3&updatedAt=1671348611035"
+//         />
+//         <meta
+//           property="og:image"
+//           content="https://ik.imagekit.io/36athv2v82c8/Screenshot_2022-12-18_125952_xWZQNyd3I.png?ik-sdk-version=javascript-1.4.3&updatedAt=1671348611035"
+//         />
+//         <meta
+//           property="og:image:url"
+//           content="https://ik.imagekit.io/36athv2v82c8/Screenshot_2022-12-18_125952_xWZQNyd3I.png?ik-sdk-version=javascript-1.4.3&updatedAt=1671348611035"
+//         />
+//         <title>ARJs Portfolio V3</title>
+//       </Head>
+//       <body>
+//         <Main />
+//         <NextScript />
+//       </body>
+//     </Html>
+//   );
+// }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import { ThemeContextProvider } from "@/context/theme";
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"], preload: true });
 
 export const metadata: Metadata = {
   title: "Atharva J | Portfolio Website",
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="!scroll-smooth">
-      <body className={inter.className}>
+      <body>
         <ThemeContextProvider>{children}</ThemeContextProvider>
       </body>
     </html>
