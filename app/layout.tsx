fe85fe0e7cfd509007cd51a8412f3553e0fde47f
Atharva Joshi <atharvaj2160@gmail.com>
2025-06-21 12:59:10 +0530

    Add vercel analytics and speed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import '../styles/globals.css';
 
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata, Viewport } from 'next';
 import React from 'react';
 import { JSX } from 'react';
@@ -90,6 +92,8 @@ export default function RootLayout({
         <div className='absolute right-[11rem] top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full bg-[#fbe2e3] blur-[10rem] dark:bg-[#946263] sm:w-[68.75rem]'></div>
         <div className='absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-[#dbd7fb] blur-[10rem] dark:bg-[#676394] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
         <ThemeContextProvider>{children}</ThemeContextProvider>
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
